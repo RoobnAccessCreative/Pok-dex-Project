3d4cabffd20d991f8f53eaaec17769a29182264e
Reuben <144438642+RoobnAccessCreative@users.noreply.github.com>
2024-02-15 17:30:43 +0000

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pd
 import requests as r
 import json
 
+API_URL = "https://pokeapi.co/api/v2/"
 
 def login(df):
   l_flag = False
@@ -49,39 +50,75 @@ def login(df):
   print(f"Welcome, {uname}")
   return uname, pword, row_index
 
-def team_display(df, row_index):
+def get_user_row(row_index):
+  df = pd.read_csv("UserData.csv")
   user_df = df.loc[row_index]
+  return user_df
+
+def team_display(row_index):
+  user_df = get_user_row(row_index)
   user_team_df = user_df[2:8]
+  party = []
   for i in user_team_df:
     print(i)
-  return user_df 
+    party.append(i)
+    stats_display(i)
 
-def stats_display(user_df):
+def stats_display(mon):
    pass
 
-def username_change(user_df, uname):
+def username_change(uname):
+  user_df = get_user_row()
   n_uname = input(": ")
   change = (f"change username from {uname} to {n_uname}? (y/n) ")
   if change.upper() == 'Y':
     user_df["Name"] = n_uname
-  return user_df
 
-def search():
+def search():#fullmatch search from input
   search = input(": ")
   try:
-     data = json.dumps(r.get("https://pokeapi.co/api/v2/pokemon/"+search.lower()).text)
-     print(data)
+    data = json.loads(r.get(API_URL+"pokemon/"+search.lower()).text)
+    image = r.get(f"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/{data['id']}.png")
+    print(data["name"])
   except:
-     print("not found")
-  
+    print("not found")
+
+def search_tag(type):#drop-down filter search
+    pokemon_in_type = json.loads(r.get(API_URL+"type/"+type).text)["pokemon"]
+    stored_pokemon = []
+    for mon in pokemon_in_type:
+        if len(stored_pokemon) < 10:
+            mon_data = mon["pokemon"]
+            stored_pokemon.append(mon_data)
+            print(mon_data)
+        else:
+           break
+
+def team_edit():
+  team_display(row_index)
+  choice = input("1 - add 2 - remove -- ")
+  match choice:
+    case '1':
+      add_mon()
+    case '2':
+      delete_mon()
+    case _:
+        print("pick one of the options bucko or im telling my G money (Sir Gavinsworth III: Duke of Earlesson and cranking 90s in Fortnite) >:(")
     
+def add_mon():
+  get_user_row()
+  search()
+  
+def delete_mon():
+  pass
 #----------------------------MAIN-PROGRAM--------------------------------------------------------------------
 
 
 df = pd.read_csv("UserData.csv")
 
-#uname, pword, row_index = login(df)
-#user_df = team_display(df, row_index)
-#stats_display(user_df)
-#user_df = username_change(user_df,uname)
+uname, pword, row_index = login(df)
+team_display(row_index)
+
+username_change(uname)
 search()
+search_tag("ground")
